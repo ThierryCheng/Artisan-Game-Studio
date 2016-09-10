2068c553752ae904339e9eec44d78227d5f297de
chenluo <cheng.2.luo@nokia.com>
2016-09-10 13:34:58 +0800

    ffff

---
+++ Violemon/Assets/Script/World/World.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+
+	private GameObject m_DirectionalLight;
+	private GameObject m_Violemon;
+	void Start () {
+		m_DirectionalLight = GameObject.Find ("Directional Light");
+		if(m_DirectionalLight == null)
+		{
+			return;
+		}
+		m_Violemon = GameObject.Find ("Violemon");
+		if(m_DirectionalLight == null)
+		{
+			return;
+		}
+		//m_DirectionalLight.transform.LookAt (m_Violemon.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_DirectionalLight.transform.Rotate(0, 20 * Time.deltaTime, 0);
+	}
+}
